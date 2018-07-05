2dc1e6d27813145c76ecc4c786b68a48e691f916
Yiheng Shu <lzw429@qq.com>
2018-07-05 18:41:23 +0800

    update

--- exp3/PTE.h
+++ exp3/PTE.h
@@ -12,12 +12,13 @@ public:
     int mark; // 1 已在主存；0 未在主存
     int memoryBlock; // 在主存上的块号
     int diskLocation; // 在磁盘上的位置
+    int modifyMark; // 1 修改过；0 未修改
 
     PTE(int n, int m, int d)
-            : number(n), mark(m), memoryBlock(0), diskLocation(d) {}
+            : number(n), mark(m), memoryBlock(0), diskLocation(d), modifyMark(0) {}
 
     PTE(int n, int ma, int me, int d)
-            : number(n), mark(ma), memoryBlock(me), diskLocation(d) {}
+            : number(n), mark(ma), memoryBlock(me), diskLocation(d), modifyMark(0) {}
 
 };
 
--- exp3/exp3.2.cpp
+++ exp3/exp3.2.cpp
@@ -7,8 +7,14 @@
 #include <bitset>
 #include <fstream>
 #include "PTE.h"
+#include <queue>
 
+#define PTE_SIZE 4 // 主存中的页表项数
+#define BLOCK_SIZE 10 // 主存块数
+#define blockSize 128 // 每块长度
 using namespace std;
+
+bool block[BLOCK_SIZE + 5];
 
 int main() {
     ifstream ifs;
@@ -17,14 +23,23 @@ int main() {
         cout << "打开文件失败" << endl;
         return 0;
     }
-    const int blockSize = 128; // 每块长度
     vector<PTE> pageTable;
+    queue<PTE *> queue;
+
     // 初始化页表
-    PTE PTE0(0, 1, 5, 11);
+    PTE PTE0(0, 1, 5, 11); // 页号，标志，主存块号，磁盘位置
+    block[5] = true;
+    queue.push(&PTE0);
     PTE PTE1(1, 1, 8, 12);
+    block[8] = true;
+    queue.push(&PTE1);
     PTE PTE2(2, 1, 9, 13);
+    block[9] = true;
+    queue.push(&PTE2);
     PTE PTE3(3, 1, 1, 21);
-    PTE PTE4(4, 0, 22);
+    block[1] = true;
+    queue.push(&PTE3);
+    PTE PTE4(4, 0, 22); // 页号，标志，磁盘位置
     PTE PTE5(5, 0, 23);
     PTE PTE6(6, 0, 121);
     pageTable.push_back(PTE0);
@@ -50,14 +65,29 @@ int main() {
                 if (pte.mark == 1) { // 存在于主存，形成绝对地址
                     memoryBlock = pte.memoryBlock;
                     absoluteAddress = memoryBlock * blockSize + unitNum;
-                    if (operation == "存") {
-                        // 置（页号）页修改标志“1”
-                    } else
-                        cout << "绝对地址: " << bitset<sizeof(int) * 3>(absoluteAddress) << endl;
+                    if (operation == "存")
+                        pte.modifyMark = 1;  // 置修改标志“1”
+                    cout << "绝对地址: " << bitset<sizeof(int) * 3>(absoluteAddress) << endl;
                 } else { // 不存在于主存，发生缺页中断
                     // 输出*页号
                     cout << "缺页异常: *" << pte.number << endl;
                     // 模拟FIFO页面调度
+                    PTE *front = queue.front();
+                    queue.pop();
+                    if (front->modifyMark) { // 队首的修改标志为1
+                        cout << "OUT " << front->number;
+                        front->modifyMark = 0;
+                        front->mark = 0;
+                        block[front->memoryBlock] = false;
+                    }
+                    queue.push(&pte);
+                    pte.mark = 1;
+                    for (int i = 0; i < blockSize; i++) {
+                        if (!block[i])
+                            block[i] = true;
+                        pte.memoryBlock = i;
+                    }
+                    cout << "IN " << pageNum;
                 }
             }
         }
